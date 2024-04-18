51b35722bacb32df045ecada767f430781b9c20f
Michael Sweeney <87456288+sweeml@users.noreply.github.com>
2024-04-18 14:09:09 +0000

    Created code to dynamically pass in file paths.

---
+++ examples/18-dynamic-file/dynamic_file/src/main.rs
@@ -0,0 +1,37 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Please provide a file path as a command line argument");
+    }
+    let file_path = &args[1];
+
+    let file = File::open(file_path);
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => {
+            match error.kind() {
+                std::io::ErrorKind::NotFound => {
+                    panic!("File not found: {}", error)
+                }
+                _ => {
+                    panic!("Error opening file: {}", error)
+                }
+            }
+        }
+    };
+    
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(error) => {
+                panic!("Error reading line: {}", error)
+            }
+        }
+    }
+}
+
